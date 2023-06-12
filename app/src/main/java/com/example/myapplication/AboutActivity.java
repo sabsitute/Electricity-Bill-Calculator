0b2dae21a421c9f56128e36dcaffb24c0f7bd36f
Sabrina <sabrinazran27@gmail.com>
2023-06-12 10:37:28 +0800

    Final Commit

--- app/src/main/java/com/example/myapplication/AboutActivity.java
+++ app/src/main/java/com/example/myapplication/AboutActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        TextView t = (TextView) findViewById(R.id.textlink);
+        t.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
